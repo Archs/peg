e31cc7081d0bdff8dce8b1c4e05776371b118134
Archs <sun@archs.tk>
2015-06-12 19:40:13 +0800

    test for BlockComment ok

--- grammars/typescript.d.ts/pixi.d.ts
+++ grammars/typescript.d.ts/pixi.d.ts
@@ -11,7 +11,7 @@ declare module PIXI {
     export class EventEmitter {
 
         listeners(event: string): Function;
-        emit(event: string, ...args: any): boolean;
+        emit(event: string, ...args: any[]): boolean;
         on(event: 'click', context?: any): EventEmitter; 
         on(event: string, fn: Function, context?: any): EventEmitter;
         once(event: string, fn: Function, context?: any): EventEmitter;
